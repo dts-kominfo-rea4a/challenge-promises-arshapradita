a9580b459f8bab43f157da882ccfbcb3dd7147cd
arshapradita <arshapradita@gmail.com>
2022-09-19 22:10:40 +0700

    make a promise

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+    let marah = 0;
+    let tidak_marah = 0;
+    for (i in theaterIXX) {
+      theaterIXX[i].hasil === "marah" ? marah++ : tidak_marah++;
+    }
+    for (i in theaterVGC) {
+      theaterVGC[i].hasil === "marah" ? marah++ : tidak_marah++;
+    }
+    return emosi === "marah" ? marah : tidak_marah;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
